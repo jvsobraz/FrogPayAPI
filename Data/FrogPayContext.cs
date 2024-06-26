fe01df4aefbdf3ea0d4a65f0d012ca88c93c9fb2
jvsobraz <jvsobraz@hotmail.com>
2024-06-26 19:04:30 -0300

    Corrigido a classe FrogPayContext.cs com as ModelBuilders inclusivas, fazendo com que a modelagem de dados fique correta.

--- Data/FrogPayContext.cs
+++ Data/FrogPayContext.cs
@@ -5,13 +5,35 @@ using FrogpayAPI.Models;
 namespace FrogPayAPI.Data
 {
 public class FrogPayContext : DbContext
-{
-    public FrogPayContext(DbContextOptions<FrogPayContext> options) : base(options) { }
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<DadosBancarios> DadosBancarios { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
 
-    public DbSet<Pessoa> Pessoas { get; set; }
-    public DbSet<DadosBancarios> DadosBancarios { get; set; }
-    public DbSet<Endereco> Enderecos { get; set; }
-    public DbSet<Loja> Lojas { get; set; }
+        public FrogPayContext(DbContextOptions<FrogPayContext> options) : base(options) { }
 
-}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pessoa>().HasKey(p => p.IdPessoa);
+        modelBuilder.Entity<DadosBancarios>().HasKey(d => d.IdDadosBancarios);
+        modelBuilder.Entity<Endereco>().HasKey(e => e.IdEndereco);
+        modelBuilder.Entity<Loja>().HasKey(l => l.IdLoja);
+
+       modelBuilder.Entity<DadosBancarios>()
+                .HasOne<Pessoa>()
+                .WithMany()
+                .HasForeignKey(d => d.IdPessoa);
+
+            modelBuilder.Entity<Endereco>()
+                .HasOne<Pessoa>()
+                .WithMany()
+                .HasForeignKey(e => e.IdPessoa);
+
+            modelBuilder.Entity<Loja>()
+                .HasOne<Pessoa>()
+                .WithMany()
+                .HasForeignKey(e => e.IdLoja);
+    }
+    }
 }
