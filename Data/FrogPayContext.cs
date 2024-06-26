64410ec03578f8f25becb8d9fdf6fa33a02a074d
jvsobraz <jvsobraz@hotmail.com>
2024-06-26 17:48:03 -0300

    Criado o Contexto de Dados no projeto

---
+++ Data/FrogPayContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using FrogPayAPI.Models;
+using FrogpayAPI.Models;
+
+public class FrogPayContext : DbContext
+{
+    public FrogPayContext(DbContextOptions<FrogPayContext> options) : base(options) { }
+
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<DadosBancarios> DadosBancarios { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Loja> Lojas { get; set; }
+
+
+}
